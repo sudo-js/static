52e6b52afc899dd4c6369857a59fb0b90bb7d70f
robrobbins <rob.robb.ns@gmail.com>
2014-01-27 17:34:23 -0800

    getHeight and width for Node

--- build/debug/static.js
+++ build/debug/static.js
@@ -116,6 +116,28 @@ Node.closestParent = function closestParent(node, sel) {
   }
   return node;
 };
+// ###getHeight
+// As there as no unified way to measure the height of a
+// node || document we abstract that here. We do not test
+// for `window` as that is an easy case to test for proir to calling this method.
+//
+// `param` {*} `node`
+// `returns`{number}
+Node.getHeight = function getHeight(node) {
+  if(Node.isDocument(node)) return node.documentElement.scrollHeight;
+  return node.offsetHeight;
+};
+// ###getWidth
+// As there as no unified way to measure the width of an
+// node || document we abstract that here.
+// Again, we do not test for `window`
+//
+// `param` {*} `node`
+// `returns`{number}
+Node.getWidth = function getWidth(node) {
+  if(Node.isDocument(node)) return node.documentElement.scrollWidth;
+  return node.offsetWidth;
+};
 // ###isDocument
 // Return truthy is the passed in node is the document object. Placed here on
 // the Node Class Object as `document` is a `Node` type.
