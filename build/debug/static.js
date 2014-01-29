ba86c3cae49b0ae5896771421e610caed14e7cba
Geoff Kinder <geoff.kinder@gmail.com>
2014-01-28 17:45:48 -0800

    Added NodeList.map(…)

--- build/debug/static.js
+++ build/debug/static.js
@@ -149,13 +149,13 @@ Node.isDocument = function isDocument(node) {
 };
 // ###forEach (NodeList)
 // This is a, hopefully, shortlived bit of syntactic sugar for the fact that,
-// at the moment, there isn't a succinct way to iterate over a NodeList such as `Array.forEach`. 
+// at the moment, there isn't a succinct way to iterate over a NodeList such as `Array.forEach`.
 // This method will simply construct a `for` loop around the passed in NodeList and call the
 // provided function with each item in it, returning the list
 //
 // `param` {nodeList} `list`
 // `param` {function} `fn`
-// `returns` {nodeList} `list` 
+// `returns` {nodeList} `list`
 NodeList.forEach = function nlforEach(list, fn) {
   var len = list.length, i;
   for (i = 0; i < len; i++) {
@@ -165,12 +165,12 @@ NodeList.forEach = function nlforEach(list, fn) {
 };
 // ###filter (NodeList)
 // Given a nodeList, push a reference to every node that passes the passed-in
-// truthy-test (fn) into an array and return the array. We cannot return a 
+// truthy-test (fn) into an array and return the array. We cannot return a
 // nodeList as this would modify the DOM.
 //
 // `param` {nodeList} `list`
-// `param` {function} `fn` 
-// `returns` {array}  
+// `param` {function} `fn`
+// `returns` {array}
 NodeList.filter = function nlfilter(list, fn) {
   var len = list.length, nodes = [], i;
   for (i = 0; i < len; i++) {
@@ -178,6 +178,24 @@ NodeList.filter = function nlfilter(list, fn) {
     if(fn(list[i])) nodes.push(list[i]);
   }
   return nodes;
+};
+// ###map (NodeList)
+// Given a nodeList, push the result of each node, after performing
+// a given operation (fn) on it, into an array and return the array.
+//
+// `param` {nodeList} `list`
+// `param` {function} `fn`
+// `returns` {array}
+NodeList.map = function nlmap(list, fn) {
+  var len    = list.length,
+      result = [],
+      node, i;
+
+  for (i = 0; i < len; i++) {
+    result.push(fn(list[i]));
+  }
+
+  return result;
 };
 // ###matches (Element)
 // Unfortunately the matchesSelector methods are all hidden behind prefixes ATM.
